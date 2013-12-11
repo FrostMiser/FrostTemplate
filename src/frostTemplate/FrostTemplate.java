f5f698cb60e48792ca73d7e9e49fae3c36e3fef1
FrostMiser <mcfrostmiser@gmail.com>
2013-12-10 23:45:40 -0600

    Added source.

---
+++ src/frostTemplate/FrostTemplate.java
@@ -0,0 +1,47 @@
+package frostTemplate;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.bukkit.ChatColor;
+
+
+public class FrostTemplate extends JavaPlugin {
+
+	
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new PlayerListener(),this);
+		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[FrostTemplate] Plugin enabled.");
+		return;
+			
+	}
+
+	public void onDisable() {
+		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[FrostTemplate] Plugin disabled.");
+	}
+	
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
+		Player p;
+		try {
+			p = this.getServer().getPlayer(sender.getName());	
+		}
+		catch (Exception e) {
+			return true;
+		}
+		
+		
+		if (cmd.getName().equalsIgnoreCase("Template") ) 
+		{	
+			p.sendMessage("Template");
+		}
+		
+		return true;
+	}
+
+
+}
---
+++ src/frostTemplate/PlayerListener.java
@@ -0,0 +1,21 @@
+package frostTemplate;
+
+
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+
+public class PlayerListener implements Listener {
+	
+
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+
+		}
+	}
+	
+}
