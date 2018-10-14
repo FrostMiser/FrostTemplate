d6980d95a041648379dec1f5bb21a0e97b71fc2e
FrostMiser <FrostMiser@users.noreply.github.com>
2018-10-14 13:19:23 -0500

    Cleanup spacing

--- src/frostTemplate/FrostTemplate.java
+++ src/frostTemplate/FrostTemplate.java
@@ -3,26 +3,21 @@ package frostTemplate;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
-
 import org.bukkit.plugin.java.JavaPlugin;
-
 import org.bukkit.ChatColor;
 
 
 public class FrostTemplate extends JavaPlugin {
 
-	
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new PlayerListener(),this);
 		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[FrostTemplate] Plugin enabled.");
-		return;
-			
+		return;		
 	}
 
 	public void onDisable() {
 		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[FrostTemplate] Plugin disabled.");
 	}
-	
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
 	{
@@ -34,14 +29,10 @@ public class FrostTemplate extends JavaPlugin {
 			return true;
 		}
 		
-		
 		if (cmd.getName().equalsIgnoreCase("Template") ) 
 		{	
 			p.sendMessage("Template");
 		}
-		
 		return true;
 	}
-
-
 }
