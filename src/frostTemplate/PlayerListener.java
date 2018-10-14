9ff54c22da35d01b5e618e69d1e465e6485288d8
FrostMiser <FrostMiser@users.noreply.github.com>
2018-10-14 13:20:10 -0500

    Cleanup spacing

--- src/frostTemplate/PlayerListener.java
+++ src/frostTemplate/PlayerListener.java
@@ -1,7 +1,5 @@
 package frostTemplate;
 
-
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -10,12 +8,10 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerListener implements Listener {
 	
-
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
 
 		}
 	}
-	
 }
